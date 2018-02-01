e2544e4aa87d9a65dd4ca8fb8d2f2f49b749a7c0
gtsofa <jlsnyule@yahoo.com>
2018-02-01 22:38:31 +0300

    add layout and modify home view

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hi tsofa!'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
