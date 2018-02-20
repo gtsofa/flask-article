93af8c9ed92f83bec5c8819aff4100e9851779b5
Julius Nyule <jlsnyule22@gmail.com>
2018-02-20 20:11:55 +0000

    add a data file as source of data

--- app.py
+++ app.py
@@ -3,12 +3,32 @@
 import os
 
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 
+Articles = Articles()
+
+# home route
 @app.route('/')
 def index():
+    #return 'Tsofa, karibu!'
     return render_template('home.html')
+    
+# about route
+@app.route('/about')
+def about():
+    return render_template('/about.html')
+    
+# articles route
+@app.route('/articles')
+def articles():
+    return render_template('/articles.html', articles = Articles)
+    
+# dynamic articles route
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('/article.html', id = id)
 
 
 if __name__ == '__main__':
---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    """create articles and store them in an array """
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'This is the description of article one. Should define all ones and zeros for this matter',
+            'author': 'Tsofa Nyule',
+            'create_date': '02-20-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'This is the description of article one. Should define all ones and zeros for this matter',
+            'author': 'Maestro Julio',
+            'create_date': '02-20-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'This is the description of article one. Should define all ones and zeros for this matter',
+            'author': 'Sarah Doe',
+            'create_date': '02-20-2018'
+        }
+        
+        ]
+        
+    return articles
