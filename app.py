71da31dded799def063eaae29e1391e7e9ecbb3c
Julius Nyule <jlsnyule22@gmail.com>
2018-02-20 18:09:40 +0000

    add flask

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # app.py
 
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,4 +12,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))#c9
